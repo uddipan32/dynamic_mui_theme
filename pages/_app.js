6add615939aeba923c18501dddffee408052e0fc
Uddipan Bhattacharyya <uddipan.bhatta7@gmail.com>
2022-07-18 14:53:40 +0530

    dynamic theme

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,44 @@
-import '../styles/globals.css'
+import App from "next/app";
+import absoluteUrl from "next-absolute-url";
+import { ThemeProvider } from "@mui/material/styles";
+import ProductContext from "../src/productContext";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+// ==== IMPORT THEMES ====
+import { theme1, theme2, theme3, theme4 } from "../styles/mui/theme";
+import "../styles/globals.css";
+
+MyApp.getInitialProps = async (appContext) => {
+  const { protocol, host } = absoluteUrl(appContext.ctx.req, "vcap.me:3000");
+  const sub_domain = host.split(".", 1);
+  const store_name = sub_domain[0];
+
+  const res = await fetch("https://dummyjson.com/products/" + store_name);
+  const productDetails = await res.json();
+  console.log(productDetails);
+  return { props: { productDetails } };
+};
+
+function MyApp({ Component, pageProps, props }) {
+  // ==== SELECT THEME FROM BACKEND ====
+  let selectedTheme = theme1;
+  if (props.productDetails) {
+    if (props.productDetails.brand === "Apple") {
+      selectedTheme = theme1;
+    } else if (props.productDetails.brand === "Samsung") {
+      selectedTheme = theme2;
+    } else if (props.productDetails.brand === "OPPO") {
+      selectedTheme = theme3;
+    } else if (props.productDetails.brand === "Huawei") {
+      selectedTheme = theme4;
+    }
+  }
+  return (
+    <ThemeProvider theme={selectedTheme}>
+      <ProductContext.Provider value={props.productDetails}>
+        <Component {...pageProps} />
+      </ProductContext.Provider>
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ styles/mui/theme.js
@@ -0,0 +1,90 @@
+import { createTheme } from "@mui/material/styles";
+import { green, purple } from "@mui/material/colors";
+
+export const theme1 = createTheme({
+  palette: {
+    mode: "light",
+    common: {
+      black: "#000",
+      white: "#fff",
+    },
+    primary: {
+      main: "#3E4CA0",
+      light: "#3E4CA0",
+      dark: "#3E4CA0",
+      contrastText: "#fff",
+    },
+    secondary: {
+      main: "#252A36",
+      light: "#252A36",
+      dark: "#252A36",
+      contrastText: "#fff",
+    },
+  },
+});
+
+export const theme2 = createTheme({
+  palette: {
+    mode: "light",
+    common: {
+      black: "#000",
+      white: "#fff",
+    },
+    primary: {
+      main: "#D4622B",
+      light: "#D4622B",
+      dark: "#D4622B",
+      contrastText: "#fff",
+    },
+    secondary: {
+      main: "#181818",
+      light: "#181818",
+      dark: "#181818",
+      contrastText: "#fff",
+    },
+  },
+});
+
+export const theme3 = createTheme({
+  palette: {
+    mode: "light",
+    common: {
+      black: "#000",
+      white: "#fff",
+    },
+    primary: {
+      main: "#141529",
+      light: "#141529",
+      dark: "#141529",
+      contrastText: "#fff",
+    },
+    secondary: {
+      main: "#686DB1",
+      light: "#686DB1",
+      dark: "#686DB1",
+      contrastText: "#fff",
+    },
+  },
+});
+
+export const theme4 = createTheme({
+  palette: {
+    mode: "light",
+    common: {
+      black: "#000",
+      white: "#fff",
+    },
+    primary: {
+      main: "#FCC135",
+      light: "#FCC135",
+      dark: "#FCC135",
+      contrastText: "#fff",
+    },
+    secondary: {
+      main: "#063E5F",
+      light: "#063E5F",
+      dark: "#063E5F",
+      contrastText: "#fff",
+    },
+  },
+});
